3e51cefca1f37e447c8808a5f4e2db63bc7258ed
Kevin Karanja <kevinkaranja098@gmail.com>
2024-03-08 15:35:36 +0300

    styling with bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import ReactDOM from 'react-dom';
+import "./index.css";
 import App from './components/App';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 ReactDOM.render(
   <App />,
