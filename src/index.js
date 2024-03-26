f802589a9db82b7c4447338506715bf40f92a9f5
Kevin Karanja <kevinkaranja098@gmail.com>
2024-03-26 14:23:48 +0300

    error in the app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import ReactDOM from 'react-dom';
 import "./index.css";
-import UserStorage  from './components/App';
+import App  from './components/App';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
 ReactDOM.render(
-  <UserStorage   />,
+  <App   />,
   document.getElementById('root')
 )
